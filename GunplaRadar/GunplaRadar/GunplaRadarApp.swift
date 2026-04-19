b2da825b79da4304843102192c266e8d1973a0c2
almashY <kshow2171@gmail.com>
2026-04-19 14:12:05 +0900

    初期設定

---
+++ GunplaRadar/GunplaRadar/GunplaRadarApp.swift
@@ -0,0 +1,17 @@
+//
+//  GunplaRadarApp.swift
+//  GunplaRadar
+//
+//  Created by almashY on 2026/04/19.
+//
+
+import SwiftUI
+
+@main
+struct GunplaRadarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
